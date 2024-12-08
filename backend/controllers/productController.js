d06c023dae11e39fe77763948b00c06ec388b8f2
Rushad <rushadmahmed@gmail.com>
2024-12-08 15:12:57 +0530

    Edit API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -86,28 +86,28 @@ const singleProduct = async (req, res) => {
 const editProduct = async (req, res) => {
   try {
     const { id, name, description, price, category, bestseller } = req.body;
-    const image1 = req.files.image1 && req.files.image1[0];
-    const image2 = req.files.image2 && req.files.image2[0];
-    const image3 = req.files.image3 && req.files.image3[0];
-    const image4 = req.files.image4 && req.files.image4[0];
-    const images = [image1, image2, image3, image4].filter(
-      (item) => item !== undefined
-    );
-    let imagesUrl = await Promise.all(
-      images.map(async (item) => {
-        let result = await cloudinary.uploader.upload(item.path, {
-          resource_type: "image",
-        });
-        return result.secure_url;
-      })
-    );
+    // const image1 = req.files.image1 && req.files.image1[0];
+    // const image2 = req.files.image2 && req.files.image2[0];
+    // const image3 = req.files.image3 && req.files.image3[0];
+    // const image4 = req.files.image4 && req.files.image4[0];
+    // const images = [image1, image2, image3, image4].filter(
+    //   (item) => item !== undefined
+    // );
+    // let imagesUrl = await Promise.all(
+    //   images.map(async (item) => {
+    //     let result = await cloudinary.uploader.upload(item.path, {
+    //       resource_type: "image",
+    //     });
+    //     return result.secure_url;
+    //   })
+    // );
     const updateData = {
       name,
       description,
       category,
       price: Number(price),
       bestseller: bestseller === "true" ? true : false,
-      image: imagesUrl,
+      // image: imagesUrl,
       date: Date.now(),
     };
     const updatedProduct = await productModel.findByIdAndUpdate(
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -28,12 +28,6 @@ productRouter.get("/list", listProducts);
 productRouter.post(
   "/edit",
   adminAuth,
-  upload.fields([
-    { name: "image1", maxCount: 1 },
-    { name: "image2", maxCount: 1 },
-    { name: "image3", maxCount: 1 },
-    { name: "image4", maxCount: 1 },
-  ]),
   editProduct
 );
 
